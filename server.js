33b6fed5734a505c4b04748b6cf78e7c072f2050
Jeremy <jeremy@jsrath.com>
2019-12-29 17:36:21 +0100

    fix: assign port from process.env

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const prettier = require('prettier');
 const express = require('express');
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 5000;
 
 async function returnHTML(url) {
   const browser = await puppeteer.launch();
@@ -63,4 +63,4 @@ app.get('/api', async (req, res) => {
   }
 });
 
-app.listen(port, () => console.log(`Listening on port ${port}!`));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
